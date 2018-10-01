06754f06c305267bec4665d86aea2e288d8455e0
Lilian D <dureylilian@gmail.com>
2018-10-01 17:34:53 +0200

    début de la correction

--- storage.py
+++ storage.py
@@ -11,7 +11,7 @@ class OverwriteStorage(FileSystemStorage):
     def _save(self, name, content):
     	if self.exists(name):
     		self.delete(name)
-    		return super(OverwriteStorage, self)._save(name, content)
+    	return super(OverwriteStorage, self)._save(name, content)
 
     def get_available_name(self, name,max_length=None):
         return name
