42ee8e77af6d60b90c90cb51eb19bbc5ffc63295
Lilian <dureylilian@gmail.com>
2019-01-15 10:19:48 +0000

    modif tous les projets et détails projet

--- collaborateurs/css/details_projet_script.js
+++ collaborateurs/css/details_projet_script.js
@@ -1,8 +1,31 @@
 
 $(function(){
 
+    $("#b1").css('color','red');
+    $(".projet_container_2").hide(0);
 
+
+    $("#b1").on('click',
+        function cache2montre1(){
+            $("#b1").css('color','red');
+            $("#b2").css('color','black');
+            $(".projet_container_1").show(0);
+            $(".projet_container_2").hide(0);
+        }
+
+      );
     
+    $("#b2").on('click',
+        function cache1montre2(){
+            $("#b1").css('color','black');
+            $("#b2").css('color','red');
+            $(".projet_container_1").hide(0);
+            $(".projet_container_2").show(0);
+        }
+
+      );
+
+
     function Redim(){
             
 
@@ -44,12 +67,25 @@ $(function(){
     );
 
 
+
+
+
+    //pour la propsérité inutile actuellement
     $("#open_map1").on('click',callMap1);
 
     $("#close_map").on('click',closeMap);
 
 });
-    
+//fin du call du DOM
+
+
+
+
+
+
+
+
+
     function callMap1(){
         $("#map1").width(450);
         $("#map1").height(300);
